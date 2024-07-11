4edc959d43bd1869c62708961a12df5bc9cfba56
Oscar Medina <oscargil80@gmail.com>
2024-07-11 00:25:40 -0400

    Terminado el Initial Septup lo estoy haciendo manual

--- app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
+++ app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
@@ -2,16 +2,30 @@ package dev.leonardom.androidintroduccion.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import dev.leonardom.androidintroduccion.R
 import dev.leonardom.androidintroduccion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController:NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
 
     }
 }
