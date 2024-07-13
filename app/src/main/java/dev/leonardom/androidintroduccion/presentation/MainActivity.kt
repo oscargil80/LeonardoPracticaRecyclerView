91fe9127ad81f8fcd2632bdb8ab0ea7663d0ad74
Oscar Medina <oscargil80@gmail.com>
2024-07-12 23:50:33 -0400

    Terminado el Initial Septup lo estoy haciendo manual

--- app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
+++ app/src/main/java/dev/leonardom/androidintroduccion/presentation/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dev.leonardom.androidintroduccion.R
 import dev.leonardom.androidintroduccion.databinding.ActivityMainBinding
@@ -15,15 +17,51 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController:NavController
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        //BottonNavigationView
+        binding.bottomNavigationView?.setupWithNavController(navController)
+
+        //Tollbar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        //NavigationRail
+        binding.navigationRailView?.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId)
+            {
+                R.id.productDetailFragment2 -> {
+                    navController.navigate(R.id.productListFragment)
+                    true
+                }
+                R.id.favoritesFragment -> {
+                    navController.navigate(R.id.favoritesFragment)
+                    true
+                }
+                R.id.shoppingCartFragment -> {
+                    navController.navigate((R.id.shoppingCartFragment))
+                    true
+                }
+                else ->false
+            }
+
+
+        }
+
+
+
+
 
 
 
